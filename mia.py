808ce02013ccdfad6c955f4cf49c82c66a1e5f66
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-06-20 22:17:20 -0500

    forma datetime

--- mia.py
+++ mia.py
@@ -1,24 +1,25 @@
 import csv
-import datetime as dt
+import datetime
 
 
 f = open("feb2017.csv", "r").read()
 rows = f.split('\n')
 
 final_data = []
-single_report = []
 
 for i in rows:
     final_data.append(i.split(','))
 
 del final_data[0] # Delete header row
 
-#for i in rows:
-#    final_data =  datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime("%m/%d/%Y")
+eric = final_data[:10]
+
+for i in eric:
+    datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime("%m/%d/%Y")
 
 #for i in final_data:
 #    trial = i[0]
 #    if trial not in single_report:
 #        single_report.append(i)
 
-print(final_data[:11])
+print(eric)
