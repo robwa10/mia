d848519713e11a29d4ae882a97bacc0cc0564537
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-06-21 10:37:04 -0500

    format date for final_data list

--- mia.py
+++ mia.py
@@ -5,6 +5,7 @@ import datetime
 f = open("feb2017.csv", "r").read()
 rows = f.split('\n')
 
+date_format = "%m-%d-%Y"
 final_data = []
 
 for i in rows:
@@ -12,20 +13,29 @@ for i in rows:
 
 del final_data[0] # Delete header row
 
-eric = final_data[:10]
-temp = []
+for i in final_data:
+    i[2] = datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format)
 
-for i in eric:
-    temp.append(i[2])
-
-date_format = "%m-%d-%Y"
-
-for i in eric:
-    temp.append(datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format))
 
 #for i in final_data:
 #    trial = i[0]
 #    if trial not in single_report:
 #        single_report.append(i)
 
-print(temp)
+
+# -------------------------------------------------------------
+# This is the sandbox area to test.
+
+eric = final_data[:10]
+temp = []
+
+for i in eric:
+    temp.append(i[2])
+
+#for i in eric:
+#    temp.append(datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format))
+
+# End of sandbox area
+# -------------------------------------------------------------
+
+print(final_data[:20])
