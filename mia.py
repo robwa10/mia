fd018dad00b694805ccf07e5628fa3d547b8b6a3
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-06-21 22:24:01 -0500

    compare date with dict key, swap higher date, add to total count

--- mia.py
+++ mia.py
@@ -11,17 +11,21 @@ del rows[0] # Delete header row
 date_format = "%m-%d-%Y"
 data = []
 final_data = dict()
-exit = 1
+dict_errors = 0 
 
 for i in rows:
     data.append(i.split(','))
 
 for i in data:
     i[2] = datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format)
-    if i[1] not in final_data:
+    if i[1] in final_data:
+        if final_data[i[1]]['date'] < i[2]:
+            final_data[i[1]]['date'] = i[2]
+        final_data[i[1]]['total'] = final_data[i[1]]['total'] + 1
+    elif i[1] not in final_data:
         final_data[i[1]] = {'first': i[0], 'last': i[1], 'date': i[2], 'total': 1}
     else:
-        exit += 1
+        dict_errors += 1
 
 #for i in data:
 #    name = i[0] + " " + i[1]
@@ -31,7 +35,7 @@ for i in data:
 #        final_data[name] = date 
 
 print(final_data)
-
+print(dict_errors)
 
 
 # -------------------------------------------------------------
