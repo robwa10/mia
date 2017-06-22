949e6c6e938c630ae0e656fe0efd7289e2939b4e
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-06-21 21:54:53 -0500

    iterate list to make 1st instance dictionary

--- mia.py
+++ mia.py
@@ -10,39 +10,33 @@ del rows[0] # Delete header row
 
 date_format = "%m-%d-%Y"
 data = []
-full_name = []
-date = []
 final_data = dict()
+exit = 1
 
 for i in rows:
     data.append(i.split(','))
 
 for i in data:
-    date = datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format)
-    name = i[0] + " " + i[1]
-    if name in final_data:
-        final_data[name] = final_data[name] + date
+    i[2] = datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format)
+    if i[1] not in final_data:
+        final_data[i[1]] = {'first': i[0], 'last': i[1], 'date': i[2], 'total': 1}
     else:
-        final_data[name] = date 
+        exit += 1
+
+#for i in data:
+#    name = i[0] + " " + i[1]
+#    if name in final_data:
+#        final_data[name] = final_data[name] + date
+#    else:
+#        final_data[name] = date 
 
 print(final_data)
 
-#del final_data[0] # Delete header row
-
-#for i in final_data:
-#    i[2] = datetime.datetime.strptime(i[2], "%m/%d/%Y").strftime(date_format)
-
-
-#for i in final_data:
-#    trial = i[0]
-#    if trial not in single_report:
-#        single_report.append(i)
 
 
 # -------------------------------------------------------------
 # This is the sandbox area to test.
 
-#print(date[:20])
 
 # End of sandbox area
 # -------------------------------------------------------------
