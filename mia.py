83921c49132cf6ab4c08dc6333c34752fd9c68eb
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-06-25 07:34:53 -0500

    Parse master list to dictionary

--- mia.py
+++ mia.py
@@ -1,14 +1,14 @@
 # ----------------------- This is 79 characters -------------------------------
 import datetime
 import csv
-
+from dateutil.parser import parse
 
 # Global Variables
 date_format = "%m/%d/%Y"
 dict_errors = 0
 
 
-def prep_file(a_file):
+def prep_file(a_file, n, secondary=False):
     """Read and split the csv on newline and comma and add to a list."""
     bar = []
     f = open(a_file, 'r').read()
@@ -16,19 +16,13 @@ def prep_file(a_file):
     for i in rows:
         new = i.split(',')
         bar.append(new)
+    if secondary == True:
+        for i in bar:
+            i[n] = datetime.datetime.strptime(
+                i[n], date_format).strftime(date_format)
     #print("This is the a_dict list.")
     #print(a_dict)
     return bar
-
-
-def format_date(foo, n):
-    """Format the dates in each list for comparison later."""
-    for i in foo:
-        i[n] = datetime.datetime.strptime(
-            i[n], date_format).strftime(date_format)
-    #print("\n\n\nThis is the data list after time formatting.")
-    #print(data)
-    return foo
 
 
 def comparison_dict(a, fir, las, dt):
@@ -50,6 +44,25 @@ def comparison_dict(a, fir, las, dt):
     return a_dict
 
 
+def master_dict(d, c, f, x):
+    m_dict = {}
+    for i in d:
+        m_dict[i[c]] = {
+            'last attendance': i[f],
+            'total': i[x]
+            }
+    return m_dict
+
+# User input of master file
+master_file = input('Master filename >')
+mas_name = 0
+mas_date = 1
+mas_total = 2
+
+# Parsing and turning the master file into a dictionary
+master_data = prep_file(master_file, mas_date)
+master_dictionary = master_dict(master_data, mas_name, mas_date, mas_total)
+
 # User input of comparison file
 secondary_file = input('comparison filename >')
 sec_fir_name = input('First Name column (e.g. 1,2,3...) >')
@@ -58,14 +71,17 @@ sec_las_name = input('Last Name column (e.g. 1,2,3...) >')
 sec_las = int(sec_las_name) - 1
 sec_date_col = input('Date column (e.g. 1,2,3...) >')
 sec_date = int(sec_date_col) - 1 
-comp_file = prep_file(secondary_file)
-new_data = format_date(comp_file, sec_date)
-sec_dict = comparison_dict(new_data, sec_fir, sec_las, sec_date)
+
+# Parsing and turning the comparison file into a dictionary
+comp_file = prep_file(secondary_file, sec_date, secondary=True)
+sec_dict = comparison_dict(comp_file, sec_fir, sec_las, sec_date)
 
 """
 w = csv.writer(open("master_list", "w"))
 for person, info in comp_file.items():
    w.writerow([person, info['last attendance'], info['total']])
 """
+print(sec_dict)
 print("You're files are waiting on you!")
 print("You're total errors in creating dictionaries were %d." % dict_errors)
+print(master_dictionary)
